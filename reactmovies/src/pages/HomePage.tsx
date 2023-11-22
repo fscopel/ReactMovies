827734aad69231891aa3d98b575a25d0963ac243
Fabio Scopel <fabioscopel99@gmail.com>
2023-11-21 17:57:09 -0800

    Re-created the project using Vite

---
+++ reactmovies/src/pages/HomePage.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { device } from "../utils/devices";
+
+export const HomePage = () => {
+  return (
+    <HomePageContainer>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of the React Movies application.</p>
+    </HomePageContainer>
+  );
+};
+
+const HomePageContainer = styled.div`
+  margin-top: 70px;
+
+  @media ${device.mobileS} {
+    margin-top: 50px;
+  }
+
+  @media ${device.mobile} {
+    margin-top: 30px;
+  }
+`;
